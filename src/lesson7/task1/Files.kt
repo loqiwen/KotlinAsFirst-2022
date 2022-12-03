c967071d580ac17377808400c8e08f9716eabf08
loqiwen <serstp04@yandex.ru>
2022-12-03 20:02:31 +0300

    bbp

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -334,7 +334,9 @@ fun markdownToHtmlSimple(inputName: String, outputName: String) {
     for (ind in taggedText.indices) {
         if (taggedText[ind].replace(Regex("""\s"""), "").isEmpty()) {
             if (lastLineNotEmpty && lastCheckNotEmpty >= 0) {
-                if (ind + 1 < taggedText.size && taggedText[ind + 1].replace(Regex("""\s"""), "").isNotEmpty()) {
+                if (ind in 0 until taggedText.size - 1 && taggedText[ind + 1].replace(Regex("""\s"""), "")
+                        .isNotEmpty()
+                ) {
                     taggedText[ind] = "</p><p>"
                     lastLineNotEmpty = false
                 }
