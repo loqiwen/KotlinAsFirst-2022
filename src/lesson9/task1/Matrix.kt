f79f86c7afde33722926433506c5247a18f26c41
loqiwen <serstp04@yandex.ru>
2022-12-08 18:42:02 +0300

    matrix

--- src/lesson9/task1/Matrix.kt
+++ src/lesson9/task1/Matrix.kt
@@ -90,7 +90,7 @@ class MatrixImpl<E>(override val height: Int, override val width: Int, e: E) : M
         for (row in 0..height - 1) {
             sb.append("[")
             for (column in 0..width) {
-                if (column != width) sb.append(Cell(row, column)) else sb.append("]")
+                if (column != width) sb.append(matrixCells[Cell(row, column)]) else sb.append("]")
             }
 
         }
