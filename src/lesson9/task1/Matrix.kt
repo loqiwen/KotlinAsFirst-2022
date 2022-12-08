d8b0fc7fac116b7a593e6ab6e05749dfa48e7e78
loqiwen <serstp04@yandex.ru>
2022-12-08 14:38:39 +0300

    -mst

--- src/lesson9/task1/Matrix.kt
+++ src/lesson9/task1/Matrix.kt
@@ -87,9 +87,9 @@ class MatrixImpl<E>(override val height: Int, override val width: Int, e: E) : M
     override fun toString(): String {
         val sb = StringBuilder()
         sb.append("[")
-        for (row in 0 until height) {
+        for (row in 0..height - 1) {
             sb.append("[")
-            for (column in 0 until width) {
+            for (column in 0..width - 1) {
                 sb.append(this[row, column])
             }
             sb.append("]")
